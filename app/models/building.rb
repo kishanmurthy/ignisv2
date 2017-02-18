95d5896771c7e07826d4ebc8f48dd535f12510af
kishann380 <kishann380@bitbucket.org>
2017-02-18 20:03:39 +0530

    Fixed a bug (in model)

--- app/models/building.rb
+++ app/models/building.rb
@@ -1,3 +1,4 @@
 class Building < ApplicationRecord
   belongs_to :firestation
+  has_many :house
 end
--- app/models/firestation.rb
+++ app/models/firestation.rb
@@ -1,3 +1,4 @@
 class Firestation < ApplicationRecord
   belongs_to :hobli
+  has_one :building
 end
